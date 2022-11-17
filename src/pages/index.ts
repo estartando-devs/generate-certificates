7006d6cfb45a33822f5e51c6437025cba155356b
jonataspinto <jonatastdb_jp@hotmail.com>
2022-11-17 11:32:50 -0300

    feat: use api to get graduated students from admin

---
+++ src/pages/index.ts
@@ -0,0 +1,10 @@
+import { api } from '../services/api';
+
+export { Home as default } from '../components/modules/_HomePage/_HomePage';
+
+export const getStaticProps = async () => {
+  const students = await api<Array<Student>>('/subscribe?graduated=true&fields=fullName,email,course');
+
+  return { props: { students } };
+};
+
