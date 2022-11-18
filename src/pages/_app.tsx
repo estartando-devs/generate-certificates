8f39fb678f4ac1c3b0e41d42f4dc659705dbe183
jonataspinto <jonatastdb_jp@hotmail.com>
2022-11-18 18:17:24 -0300

    feat: implements progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import NextNProgress from 'nextjs-progressbar';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles, theme } from '../styles';
@@ -6,8 +7,14 @@ import { Layout } from '../components/modules';
 const App = ({ Component, pageProps }: AppProps) => (
   // @ts-ignore
   <ThemeProvider theme={theme}>
-  {/* @ts-ignore */}
+    {/* @ts-ignore */}
     <GlobalStyles />
+    <NextNProgress
+      color="#81CAA8"
+      startPosition={0.3}
+      stopDelayMs={200}
+      height={3}
+    />
     <Layout>
       <Component {...pageProps} />
     </Layout>
