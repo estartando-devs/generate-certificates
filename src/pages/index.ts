fb995dcfb741ac9b3f3d13c687287373dd32aad2
jonataspinto <jonatastdb_jp@hotmail.com>
2022-11-21 17:13:56 -0300

    feat: transform home page  in SSR

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,7 +2,7 @@ import { api } from '../services/api';
 
 export { Home as default } from '../components/modules/_HomePage/_HomePage';
 
-export const getStaticProps = async () => {
+export const getServerSideProps = async () => {
   const students = await api<Array<Student>>('/subscribe?graduated=true&fields=fullName,email,course');
 
   return { props: { students } };
