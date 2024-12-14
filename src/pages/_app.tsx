8c117d60d998d4e49c2f97d9c57cb0be70df6cb7
Ramon Xavier <ramon1234rxm@gmail.com>
2024-12-14 12:16:17 -0300

    feat: add confetti animations

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,37 @@ import { DefaultSeo } from 'next-seo';
 import SEO from '../../next-seo.config';
 import { GlobalStyles, theme } from '../styles';
 import { Layout } from '../components/modules';
+import { useEffect } from 'react';
+import confetti from 'canvas-confetti';
 
-const App = ({ Component, pageProps }: AppProps) => (
+const App = ({ Component, pageProps }: AppProps) =>{ 
+  useEffect(() => {
+    const end = Date.now() + (15 * 100);
+    const colors = ['#bb0000', '#ffffff'];
+    
+    (function frame() {
+      confetti({
+        particleCount: 2,
+        angle: 60,
+        spread: 55,
+        origin: { x: 0 },
+        colors: colors
+      });
+      confetti({
+        particleCount: 2,
+        angle: 120,
+        spread: 55,
+        origin: { x: 1 },
+        colors: colors
+      });
+    
+      if (Date.now() < end) {
+        requestAnimationFrame(frame);
+      }
+    }());
+  }, []); 
+
+  return (
   // @ts-ignore
   <ThemeProvider theme={theme}>
     {/* @ts-ignore */}
@@ -22,6 +51,6 @@ const App = ({ Component, pageProps }: AppProps) => (
       <Component {...pageProps} />
     </Layout>
   </ThemeProvider>
-);
+)};
 
 export default App;
--- src/pages/certificates/[id].tsx
+++ src/pages/certificates/[id].tsx
@@ -6,7 +6,7 @@ import { NextSeo } from 'next-seo';
 import capitalize from '../../utils/capitalize';
 import { students } from '_mock/students2022';
 import { useRouter } from 'next/router';
-
+import confetti from 'canvas-confetti';
 
 const coursesInfos = {
   frontend: {
@@ -38,7 +38,7 @@ const keyByCourse: Record<Course, CourseKeys> = {
 };
 
 const Certificate = () => {
-  const router = useRouter()
+  const router = useRouter();
   const student = students.find((student) => student.id === router.query?.id);
 
   const {
@@ -46,15 +46,50 @@ const Certificate = () => {
     handleDownloadImage,
     loading,
   } = useDownloadContainerAsImage();
-  
+
   useEffect(() => {
     if (containerRef.current && !loading) {
       containerRef.current.style.boxShadow = `0px 0px 30px ${color}`;
     }
   }, [loading]);
 
-  if (!student) return null
-   
+  useEffect(() => {
+    const duration = 15 * 1000;
+    const animationEnd = Date.now() + duration;
+    const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+    function randomInRange(min: number, max: number) {
+      return Math.random() * (max - min) + min;
+    }
+
+    const interval = setInterval(function () {
+      const timeLeft = animationEnd - Date.now();
+
+      if (timeLeft <= 0) {
+        return clearInterval(interval);
+      }
+
+      const particleCount = 50 * (timeLeft / duration);
+      // since particles fall down, start a bit higher than random
+      confetti({
+        ...defaults,
+        particleCount,
+        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+      });
+      confetti({
+        ...defaults,
+        particleCount,
+        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+      });
+    }, 250);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  if (!student) return null;
+
   const courseOption: TCourseOptions = keyByCourse?.[student.data.course];
 
   const courseSelected = coursesInfos?.[courseOption] ?? {};
@@ -109,6 +144,5 @@ const Certificate = () => {
     </>
   );
 };
-
 
 export default Certificate;
